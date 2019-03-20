1a92eb8c5c4a3b41c52132d297858901c6bc6295
Robert Tate <btate712@gmail.com>
2019-03-20 00:33:22 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,34 @@
+class Genre
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def artists
+    artists = []
+    @songs.each { |song| artists << song.artist if !artists.include?(song.artist) }
+    artists
+  end
+
+  def self.create(genre_name)
+    genre = new(genre_name)
+    genre.save
+    genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name, initializing_artist = nil, initializing_genre = nil)
+    @name = name
+    self.genre = initializing_genre
+    self.artist = initializing_artist
+  end
+
+  def save
+    @@all << self
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) if @artist != nil
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self if @genre != nil && !genre.songs.include?(self)
+  end
+
+  def self.create(song_name)
+    song = new(song_name)
+    song.save
+    song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+end
