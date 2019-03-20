29c1b9150990b9f8befcde37c8f241db30eae455
Robert Tate <btate712@gmail.com>
2019-03-20 00:37:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,4 +41,10 @@ class Song
   def self.find_by_name(name)
     @@all.find { |song| song.name == name }
   end
+
+  def find_or_create_by_name(name)
+    song = find_by_name(name)
+    song = create(name) if song == nil
+    song
+  end
 end
